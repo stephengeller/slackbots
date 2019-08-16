05105b0344c69e94f66b0df7bd13baa17646deb2
Stephen Geller <stephen.geller+git@gmail.com>
2019-08-16 14:25:40 +0100

    Create prompt before shutting down EC2 instance, cleanup index.js

--- minecraft-slackbot/src/index.js
+++ minecraft-slackbot/src/index.js
@@ -1,14 +1,19 @@
 const AWS = require("aws-sdk");
 const axios = require("axios");
 require("dotenv").config();
+const { INSTANCE_IP, SERVER_INSTANCE_ID } = process.env;
+const mcApiUrl = "https://mcapi.us/server/status?port=25565&ip=" + INSTANCE_IP;
 
 AWS.config.update({ region: "eu-west-2" });
 
-// const stephenID = "ULQRZJEK1";
+const abortStopText =
+  "*Stop aborted*\n" +
+  "Please log into the server and run `/stop` to shut down the server, " +
+  "then run `/minecraft stop` again.";
+
 const minecraftChannel = "CLBLVNLE7";
-const { INSTANCE_IP, SERVER_INSTANCE_ID } = process.env;
 const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
-const params = {
+const ec2Params = {
   DryRun: false,
   InstanceIds: [SERVER_INSTANCE_ID]
 };
@@ -37,15 +42,11 @@ const jsonBlockResponse = (channel, blocks, response_type = "in_channel") => {
   };
 };
 
-// function userIsValid(user) {
-//   return user === stephenID;
-// }
-
 const turnOnServer = async user => {
   const { state } = await getEC2State();
   if (state === "stopped") {
     await ec2
-      .startInstances(params)
+      .startInstances(ec2Params)
       .promise()
       .catch(err => err);
     return `<@${user}> turned on the server!`;
@@ -54,13 +55,13 @@ const turnOnServer = async user => {
   }
 };
 
-function promptStopConfirmation(user) {
+function stopConfirm(user) {
   return [
     {
       type: "section",
       text: {
         type: "mrkdwn",
-        text: `*<@${user}>, have you shut down the server with \`/stop\`?*`
+        text: `*<@${user}>, have you gracefully stopped the server with \`/stop\`?*`
       }
     },
     {
@@ -83,24 +84,34 @@ function promptStopConfirmation(user) {
   ];
 }
 
-const turnOffServer = async user => {
+const stopEC2Server = async user => {
+  await ec2
+    .stopInstances(ec2Params)
+    .promise()
+    .catch(err => err);
+  return `<@${user}> is stopping the server!`;
+};
+
+const promptStopConfirmation = async user => {
   const { state } = await getEC2State();
   if (state === "stopped" || state === "stopping") {
-    return "Server is *off*\nCheck `/minecraft status`.";
+    return [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Server is *off*\nCheck `/minecraft status`."
+        }
+      }
+    ];
   } else {
-    return promptStopConfirmation(user);
-
-    // await ec2
-    //   .stopInstances(params)
-    //   .promise()
-    //   .catch(err => err);
-    // return `<@${user}> is stopping the server!`;
+    return stopConfirm(user);
   }
 };
 
 async function getEC2State() {
   return await ec2
-    .describeInstances(params)
+    .describeInstances(ec2Params)
     .promise()
     .then(data => {
       const serverInfo = data.Reservations[0].Instances[0];
@@ -114,6 +125,22 @@ async function getEC2State() {
     .catch(err => console.log(err));
 }
 
+function assertServerState(data, ec2Info) {
+  if (data.online) {
+    return (
+      "\nServer is *online*!\n" +
+      `*${data.players.now} players* currently online.\n`
+    );
+  } else {
+    return (
+      `\nEC2 instance is *${
+        ec2Info.state
+      }* but *Minecraft server is not running*.\nWait for a few seconds if just turning it on, then speak to Stephen?\n` +
+      ec2Info.message
+    );
+  }
+}
+
 const serverStatus = async () => {
   const ec2Info = await getEC2State();
 
@@ -125,52 +152,80 @@ const serverStatus = async () => {
     );
   }
 
-  const url = "https://mcapi.us/server/status?port=25565&ip=" + INSTANCE_IP;
-  return await axios.get(url).then(res => {
-    let formattedStr;
+  return await axios.get(mcApiUrl).then(res => {
     const { data } = res;
-    if (data.online) {
-      formattedStr = "\nServer is *online*!\n";
-      formattedStr += `*${data.players.now} players* currently online.\n`;
-      return formattedStr;
-    } else {
-      return (
-        `\nEC2 instance is ${
-          ec2Info.state
-        } but *Minecraft server is not running*.\nWait for a few seconds if just turning it on, then speak to Stephen?\n` +
-        ec2Info.message
-      );
-    }
+    return assertServerState(data, ec2Info);
   });
 };
 
+const logAxiosErr = err => {
+  console.log("axios err:");
+  console.log(err);
+};
+
+const axiosResponse = async (response_url, params) => {
+  return await axios.post(response_url, params).catch(err => {
+    logAxiosErr(err);
+    throw err;
+  });
+};
+
+async function handlePayload(parsed) {
+  let params;
+  const payload = JSON.parse(parsed.get("payload"));
+  const { response_url, user, actions } = payload;
+  const { value } = actions[0];
+
+  if (value === "yes") {
+    params = {
+      text: await stopEC2Server(user.id),
+      response_type: "in_channel"
+    };
+  } else if (value === "no") {
+    params = {
+      text: abortStopText,
+      response_type: "ephemeral"
+    };
+  } else {
+    params = {
+      text: "*Action not understood :(*",
+      response_type: "ephemeral"
+    };
+  }
+  return await axiosResponse(response_url, params);
+}
+
 const handler = async event => {
   let msg = "Incorrect arg, try `/minecraft on|off|status`";
-  msg = JSON.stringify(event);
 
   try {
-    const { body } = event;
-    const parsed = new URLSearchParams(body);
-    const req_channel = parsed.get("channel_id");
-    const user_id = parsed.get("user_id");
-    const command = parsed.get("command");
-    const text = parsed.get("text");
+    const parsed = new URLSearchParams(event.body);
+    if (parsed.get("payload") != null) {
+      return await handlePayload(parsed, event);
+    }
+
+    const [req_channel, user_id, command, text] = [
+      parsed.get("channel_id"),
+      parsed.get("user_id"),
+      parsed.get("command"),
+      parsed.get("text")
+    ];
+
     if (command === "/minecraft") {
       if (text === "on" || text === "start") {
         msg = await turnOnServer(user_id);
+        return jsonResponse(minecraftChannel, msg, "ephemeral");
       } else if (text === "off" || text === "stop") {
-        const blocks = await turnOffServer(user_id);
+        const blocks = await promptStopConfirmation(user_id);
         return jsonBlockResponse(minecraftChannel, blocks);
       } else if (text === "status") {
         msg = await serverStatus();
         return jsonResponse(req_channel, msg, "ephemeral");
       }
     }
-    return jsonResponse(minecraftChannel, msg, "in_channel");
   } catch (e) {
     msg = "ERROR: " + e.toString();
     console.log(msg);
-    console.log(event);
     return {
       statusCode: 200,
       body: JSON.stringify({
